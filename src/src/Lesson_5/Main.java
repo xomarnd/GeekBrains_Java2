c4cffef2f73b996f32d6bfccc6b4be1ffe2a9738
xomarnd <smirnov.inv@gmail.com>
2019-10-21 13:22:39 +0300

    Lesson 4 HW

--- src/src/Lesson_5/Main.java
+++ src/src/Lesson_5/Main.java
@@ -1,7 +1,77 @@
 package Lesson_5;
 
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Lesson 5!");
+    private static final int size = 10000000;
+    private static final int h = size / 2;
+
+    public static void main(String[] args){
+        Main mainClass = new Main ();
+        mainClass.methodOne();
+        mainClass.methodTwo();
+    }
+
+    private void calculate(float[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+
+    private void methodOne(){
+        System.out.println("Метод 1 - ПУСК");
+
+        float[] arr = new float[size];
+        Arrays.fill(arr, 1f);
+        long start = System.currentTimeMillis();
+        calculate(arr);
+        long stop = System.currentTimeMillis();
+        System.out.println(String.format("Метод 1 - СТОП, время выполнения %s \n", String.valueOf(stop - start)));
+    }
+
+    private void methodTwo(){
+        System.out.println("Метод 2 - ПУСК");
+
+        float[] arr = new float[size];
+        float[] arr1 = new float[h];
+        float[] arr2 = new float[h];
+        Arrays.fill(arr, 1f);
+        long start = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+
+        long split = System.currentTimeMillis();
+        System.out.println(String.format("Создание массивов - %s \n", String.valueOf(split - start)));
+
+        Thread thread1 = new Thread(() ->this.methodTwoCalculate (arr1, 1));
+        Thread thread2 = new Thread(() ->this.methodTwoCalculate (arr2, 2));
+
+        thread1.start();
+        thread2.start();
+
+        try{
+            thread1.join();
+            thread2.join();
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+
+
+        long connect = System.currentTimeMillis();
+        System.arraycopy(arr1, 0, arr, 0, h);
+        System.arraycopy(arr2, 0, arr, h, h);
+        long end = System.currentTimeMillis();
+
+        System.out.println(String.format("Склейка массивов - %s", String.valueOf(end - connect)));
+        System.out.println(String.format("Метод 2 - СТОП, время выполнения %s \n", String.valueOf(end - start)));
+    }
+
+    private void methodTwoCalculate(float[] arr, int number){
+        long start = System.currentTimeMillis();
+        calculate(arr);
+        long end = System.currentTimeMillis();
+
+        System.out.println(String.format("Поток %d работал %s", number, String.valueOf(end - start)));
     }
 }
